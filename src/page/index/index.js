adbaae652f5401742feec4a7f97e61de9242488c
cnlxc <821382992@qq.com>
2019-06-22 20:29:53 +0900

    index page banner

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -4,7 +4,19 @@ var _css = require('./index.css');
 var _mm = require('../../util/mm.js');
 var navi_simple = require('../common/navi-simple/index.js');
 var header = require('../common/header/index.js');
-var nav_side = require('../common/nav-side/index.js');
-nav_side.init({name : 'user-center'});
+var slider = require('../../util/slider/index.js')
 var footer = require('../common/footer/footer.css');
 
+var bannerTemplate = require('./banner.string');
+
+$(function() {
+    var htmlBannerTemplate = _mm.renderHtml(bannerTemplate);
+    $('.banner-con').html(htmlBannerTemplate);
+    var $unslider = $('.banner').unslider({dots:true});
+    $('.banner-con .banner-arrow').click(
+        function(){
+            var forward = $(this).hasClass('prev')?'prev':'next';
+            $unslider.data('unslider')[forward]();
+        }
+    );
+});
